b129b2a2e76e2ca09e3970ec52fd349cb807b8a8
E.A. DAEHNHARDT <edaehn@EAs-MacBook-Air.local>
2018-11-28 10:05:24 +0100

    +public and private key pairs

--- notes.py
+++ notes.py
@@ -2,21 +2,86 @@
 # __*__ coding: utf-8 _*_
 
 # Dependencies:
-# 1. brew:
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# 2. geckodriver:
-# brew install geckodriver
+# pip install pycrypto
 
-import sys, keyring, keyring.backend, base64, clipboard
+
+from Crypto.PublicKey import RSA
+
+import sys, keyring, keyring.backend, base64, clipboard, datetime, base64
 from splinter import Browser
+from base64 import b64decode
 
-# Styling
+import glob
+import os
+import ConfigParser, io
+import string, random
+
+# Defining the directory for storing private keys ========================================
+
+
+config_file="config.ini"
+if os.path.exists(config_file): 
+	with open(config_file) as f:
+		config_contents = f.read()
+		config = ConfigParser.RawConfigParser(allow_no_value=True)
+		config.readfp(io.BytesIO(config_contents))
+else:
+    f = open(config_file, 'w')
+    config = ConfigParser.ConfigParser()
+    config.add_section('keys')
+    config.set('keys', 'public_path', './keys')
+    config.set('keys', 'private_path', '/Volumes/KEY/keys')
+    config.add_section('passwords')
+    config.set('passwords', 'length', '12')
+    config.write(f)
+    f.close()
+
+public_keys_dir=config.get('keys', 'public_path')		
+private_keys_dir=config.get('keys', 'private_path')	
+password_length=int(config.get('passwords', 'length'))	
+print(private_keys_dir)
+
+if not os.path.exists(public_keys_dir):
+	os.makedirs(public_keys_dir)
+if not os.path.exists(private_keys_dir):
+	os.makedirs(private_keys_dir)
+
+# Styling ================================================================================
 def color_it(content, style=False):
 	styles={"greenbg":'\33[102m',"red":'\33[91m', "blue":'\33[94m', "bw":'\33[7m', "green":'\33[92m'}
 	if not style or style not in styles.keys(): return content
 	return styles[style]+content+'\33[0m'
 
-# Printing table
+# Encryption =============================================================================
+from Crypto.PublicKey import RSA
+
+def generate_new_key():
+
+	private_key = RSA.generate(1024)	
+	public_key = private_key.publickey()
+
+	now=str(datetime.datetime.now().microsecond)
+
+	with open (private_keys_dir+"/private_key_"+now+".pem", "wb") as f:
+		f.write("{0}".format(private_key.exportKey()))
+
+	with open (public_keys_dir+"/public_key_"+now+".pem", "wb") as f:
+		f.write("{0}".format(public_key.exportKey()))
+	
+	try:
+		os.remove("notes.txt")
+		print "notes.txt was removed"	
+	except:
+		print "could not remove notes.txt"
+	return public_key, private_key
+    
+def get_latest_keys_file_number():
+	list_of_files = glob.glob(public_keys_dir+'/*') 
+	if len(list_of_files)==0: return False
+	latest_file = max(list_of_files, key=os.path.getctime)
+	return latest_file.split(".")[1].split("_")[2]
+
+# Printing tables ========================================================================
 def table(dict, title=False, style="greenbg"):
 	print color_it("")
 	max_length=0
@@ -40,13 +105,30 @@ def table(dict, title=False, style="greenbg"):
 class Secrets(keyring.backend.KeyringBackend):
 	_keys={}
 	_links={"google":"https:accounts.google.com"}
+	_private_key=""
+	_public_key=""
 	
-	def __init__(self):
+	def __init__(self, new=False):
+		#return
+		if new:
+			generate_new_key()
+		
+		key_file=get_latest_keys_file_number()
+		if not key_file:
+			generate_new_key()
+			key_file=get_latest_keys_file_number()
+			
+		print color_it("Using %s key file"%key_file, "blue")
+		self._private_key = RSA.importKey(open(private_keys_dir+"/private_key_"+key_file+".pem", "rb"))
+		self._public_key = RSA.importKey(open(public_keys_dir+"/public_key_"+key_file+".pem", "rb"))
 		try:
 		
-			with open('notes.txt', 'r') as f:
+			with open('notes.txt', 'rb') as f:
 				for line in f:
-					service, user, password = base64.decodestring(line).strip().split(':')
+					text=self._private_key.decrypt(b64decode(line)) 
+					#print "THIS",text
+					service, user, password = text.strip().split(':')
+
 					self._keys[(service, user)] = password
 
 
@@ -56,6 +138,10 @@ class Secrets(keyring.backend.KeyringBackend):
 	def clear(self):
 		self._keys.clear()
 		self.update_all()
+		self._public_key, self._private_key=generate_new_key()
+	
+	
+	
 	
 	def delete(self, service, user):
 		try:
@@ -81,27 +167,47 @@ class Secrets(keyring.backend.KeyringBackend):
 	
 	def update_all(self):
 		try:
-			with open("notes.txt", "w") as f:
+			with open("notes.txt", "wb") as f:
 				for key, value in self._keys.items():
-					f.write(base64.encodestring(key[0] +":" +key[1]  +":" +value))
+					#f.write(base64.encodestring(key[0] +":" +key[1]  +":" +value))
+					#print "OK:",key[0] +":" +key[1]  +":" +value+'\n'
+					text=self._public_key.encrypt(key[0] +":" +key[1]  +":" +value, 57)
+					text=base64.b64encode(text[0])
+					f.write(text+'\n')
 		except Exception, e:
 			print "Could not save! ", str(e)
 	
 	def update_key(self, service, user, password):
 		self._keys[(service, user)]=password
 		self.update_all()
+
+	def generate_password(self):
+		characters = string.ascii_letters + string.digits + '!@#$%^&*()'
+		randomness = random.SystemRandom()
+		return ''.join(randomness.choice(characters) for i in range(password_length))
 	
 	def set_password(self, service, user, password):
 		# keyring.set_password('twitter', 'ElenaD__', getpass.getpass())
 		# keyring.set_password(service, user, password)
-        
+		
+		if password=="":
+			password=self.generate_password()
+			print color_it("New password: %s"%password, "blue")
+			clipboard.copy(password)
+        	
 		if (service, user) in self._keys:
 			return self.update_key(service, user, password)
 			
 		self._keys[(service, user)]=password
 		try:
-			with open("notes.txt", "a") as f:
-				f.write(base64.encodestring(service+":"+user+":"+password))
+			with open("notes.txt", "ab") as f:
+				#f.write(base64.encodestring(service+":"+user+":"+password))
+				#print "THIS:",service+":"+user+":"+password+'\n'
+				text=self._public_key.encrypt(service+':'+user+':'+password, 57)
+				text=base64.b64encode(text[0])
+				f.write(text+'\n')
+				#self._public_key.encrypt(service+":"+user+":"+password+"\n", 57)
+				#self._public_key.encrypt(service+":"+user+":"+password+"\n", 57)
 				
 			print "Saved: %s"%(str((service, user)))
 		except Exception, e:
@@ -128,6 +234,7 @@ class Secrets(keyring.backend.KeyringBackend):
 		d['Set a key             		']="notes.py service key password"
 		d['Delete a key             	']="notes.py delete key"
 		d['Delete all keys            	']="notes.py clear"
+		d['Creating new key files       ']="notes.py new"
 		print table(d, "Command line arguments")
 		
 	def show_all(self):
@@ -135,7 +242,12 @@ class Secrets(keyring.backend.KeyringBackend):
 	
 
 def main(argv):
-	notes=Secrets()
+
+	if argv[0] in ["new", "n"] and len(argv)==1:
+		notes=Secrets(new=True)
+		notes.clear()
+	else:
+		notes=Secrets(new=False)
 	keyring.set_keyring(notes)
 	# deleting all keys from the keyring
 	if len(argv)==0:
@@ -151,6 +263,8 @@ def main(argv):
 	# deleting one key from the keywring
 	elif argv[0] in ["delete","del"] and len(argv)==3:
 		notes.delete(argv[1],argv[2])
+	elif len(argv)==3 and argv[2] in ["gen", "generate", "-p", "password"]:
+		print notes.set_password(argv[0],argv[1], "")
 	elif len(argv)==3 and '-c' not in argv:
 		print notes.set_password(argv[0],argv[1], argv[2])
 	elif len(argv)==3:
